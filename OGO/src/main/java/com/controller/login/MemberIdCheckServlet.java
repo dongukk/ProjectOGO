9bb9f95f8db686700a2c985f21f32426e4263384
Imdeuk Cho <whdlaemr1@naver.com>
2022-08-12 15:37:25 +0900

    아이디 중복 버그 수정, 로그인 파일정리(로그인페이지 새창 버전 삭제, 현재모달창)

--- OGO/src/main/java/com/controller/login/MemberIdCheckServlet.java
+++ OGO/src/main/java/com/controller/login/MemberIdCheckServlet.java
@@ -1,18 +1,13 @@
 package com.controller.login;
 
-import java.awt.Button;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.jasper.tagplugins.jstl.core.Out;
 
 import com.service.login.MemberService;
 
@@ -22,6 +17,7 @@ public class MemberIdCheckServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+
 		String userId= request.getParameter("userId");
 		System.out.println(userId);
 		
@@ -29,10 +25,10 @@ public class MemberIdCheckServlet extends HttpServlet {
 		int count = service.idCheck(userId);
 		System.out.println("아이디 중복갯수"+count);
 		
-		request.setAttribute("count", count);
-		request.setAttribute("userId", userId);
-		RequestDispatcher dis = request.getRequestDispatcher("LoginCURD/idCheck1.jsp");
-		dis.forward(request, response);
+		
+		// 넘겨줄 data
+				PrintWriter out = response.getWriter();
+				out.print(count);
 		
 	}
 
