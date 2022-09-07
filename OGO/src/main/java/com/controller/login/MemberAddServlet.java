966a0d3d518c22e1ec4878c7ce66fae28682503c
Imdeuk Cho <whdlaemr1@naver.com>
2022-09-07 15:39:19 +0900

    회원가입후 페이지이동 경로수정

--- OGO/src/main/java/com/controller/login/MemberAddServlet.java
+++ OGO/src/main/java/com/controller/login/MemberAddServlet.java
@@ -101,7 +101,7 @@ public class MemberAddServlet extends HttpServlet {
 				session.setAttribute("memberAdd",nickname+"님 회원가입성공");
 				session.setAttribute("login",dto);
 				session.setMaxInactiveInterval(60*30);
-				response.sendRedirect("MainForm2.jsp");
+				response.sendRedirect("MainForm.jsp");
 			}	
 	}
 
