d0779558b65099df131b51f0bf72fc07d7594175
ADMIN <ADMIN@DESKTOP-QMNI9RA>
2022-09-21 18:19:44 +0900

    문자 오류 수정 및 결제 내역 페이지 구현

--- OGO/src/main/java/com/dto/pay/PayDTO.java
+++ OGO/src/main/java/com/dto/pay/PayDTO.java
@@ -1,7 +1,8 @@
 package com.dto.pay;
 
 public class PayDTO {
-	int ORDERNUM;
+	
+	String ORDERNUM;
 	String USERID;
 	int CLASSNUM;
 	int PRICE;
@@ -23,17 +24,19 @@ public class PayDTO {
 	String PLACE;
 	String CLASSNAME;
 	String USERNAME;
+	String CLASSPHOTO1;
 	
-	
-	
+	String Allschedule;
+
 	public PayDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public PayDTO(int oRDERNUM, String uSERID, int cLASSNUM, int pRICE, String oRDERDATE, String oRDERSTATUS,
+
+	public PayDTO(String oRDERNUM, String uSERID, int cLASSNUM, int pRICE, String oRDERDATE, String oRDERSTATUS,
 			String sCHEDULE1, String sCHEDULE2, String sCHEDULE3, String sCHEDULE4, String sCHEDULE5, String sCHEDULE6,
 			String sCHEDULE7, String sCHEDULE8, String sCHEDULE9, String sCHEDULE10, String pLACE, String cLASSNAME,
-			String uSERNAME) {
+			String uSERNAME, String cLASSPHOTO1, String allschedule) {
 		super();
 		ORDERNUM = oRDERNUM;
 		USERID = uSERID;
@@ -54,7 +57,10 @@ public class PayDTO {
 		PLACE = pLACE;
 		CLASSNAME = cLASSNAME;
 		USERNAME = uSERNAME;
+		CLASSPHOTO1 = cLASSPHOTO1;
+		Allschedule = allschedule;
 	}
+
 	@Override
 	public String toString() {
 		return "PayDTO [ORDERNUM=" + ORDERNUM + ", USERID=" + USERID + ", CLASSNUM=" + CLASSNUM + ", PRICE=" + PRICE
@@ -62,124 +68,182 @@ public class PayDTO {
 				+ ", SCHEDULE2=" + SCHEDULE2 + ", SCHEDULE3=" + SCHEDULE3 + ", SCHEDULE4=" + SCHEDULE4 + ", SCHEDULE5="
 				+ SCHEDULE5 + ", SCHEDULE6=" + SCHEDULE6 + ", SCHEDULE7=" + SCHEDULE7 + ", SCHEDULE8=" + SCHEDULE8
 				+ ", SCHEDULE9=" + SCHEDULE9 + ", SCHEDULE10=" + SCHEDULE10 + ", PLACE=" + PLACE + ", CLASSNAME="
-				+ CLASSNAME + ", USERNAME=" + USERNAME + "]";
+				+ CLASSNAME + ", USERNAME=" + USERNAME + ", CLASSPHOTO1=" + CLASSPHOTO1 + ", Allschedule=" + Allschedule
+				+ "]";
 	}
-	public int getORDERNUM() {
+
+	public String getORDERNUM() {
 		return ORDERNUM;
 	}
-	public void setORDERNUM(int oRDERNUM) {
+
+	public void setORDERNUM(String oRDERNUM) {
 		ORDERNUM = oRDERNUM;
 	}
+
 	public String getUSERID() {
 		return USERID;
 	}
+
 	public void setUSERID(String uSERID) {
 		USERID = uSERID;
 	}
+
 	public int getCLASSNUM() {
 		return CLASSNUM;
 	}
+
 	public void setCLASSNUM(int cLASSNUM) {
 		CLASSNUM = cLASSNUM;
 	}
+
 	public int getPRICE() {
 		return PRICE;
 	}
+
 	public void setPRICE(int pRICE) {
 		PRICE = pRICE;
 	}
+
 	public String getORDERDATE() {
 		return ORDERDATE;
 	}
+
 	public void setORDERDATE(String oRDERDATE) {
 		ORDERDATE = oRDERDATE;
 	}
+
 	public String getORDERSTATUS() {
 		return ORDERSTATUS;
 	}
+
 	public void setORDERSTATUS(String oRDERSTATUS) {
 		ORDERSTATUS = oRDERSTATUS;
 	}
+
 	public String getSCHEDULE1() {
 		return SCHEDULE1;
 	}
+
 	public void setSCHEDULE1(String sCHEDULE1) {
 		SCHEDULE1 = sCHEDULE1;
 	}
+
 	public String getSCHEDULE2() {
 		return SCHEDULE2;
 	}
+
 	public void setSCHEDULE2(String sCHEDULE2) {
 		SCHEDULE2 = sCHEDULE2;
 	}
+
 	public String getSCHEDULE3() {
 		return SCHEDULE3;
 	}
+
 	public void setSCHEDULE3(String sCHEDULE3) {
 		SCHEDULE3 = sCHEDULE3;
 	}
+
 	public String getSCHEDULE4() {
 		return SCHEDULE4;
 	}
+
 	public void setSCHEDULE4(String sCHEDULE4) {
 		SCHEDULE4 = sCHEDULE4;
 	}
+
 	public String getSCHEDULE5() {
 		return SCHEDULE5;
 	}
+
 	public void setSCHEDULE5(String sCHEDULE5) {
 		SCHEDULE5 = sCHEDULE5;
 	}
+
 	public String getSCHEDULE6() {
 		return SCHEDULE6;
 	}
+
 	public void setSCHEDULE6(String sCHEDULE6) {
 		SCHEDULE6 = sCHEDULE6;
 	}
+
 	public String getSCHEDULE7() {
 		return SCHEDULE7;
 	}
+
 	public void setSCHEDULE7(String sCHEDULE7) {
 		SCHEDULE7 = sCHEDULE7;
 	}
+
 	public String getSCHEDULE8() {
 		return SCHEDULE8;
 	}
+
 	public void setSCHEDULE8(String sCHEDULE8) {
 		SCHEDULE8 = sCHEDULE8;
 	}
+
 	public String getSCHEDULE9() {
 		return SCHEDULE9;
 	}
+
 	public void setSCHEDULE9(String sCHEDULE9) {
 		SCHEDULE9 = sCHEDULE9;
 	}
+
 	public String getSCHEDULE10() {
 		return SCHEDULE10;
 	}
+
 	public void setSCHEDULE10(String sCHEDULE10) {
 		SCHEDULE10 = sCHEDULE10;
 	}
+
 	public String getPLACE() {
 		return PLACE;
 	}
+
 	public void setPLACE(String pLACE) {
 		PLACE = pLACE;
 	}
+
 	public String getCLASSNAME() {
 		return CLASSNAME;
 	}
+
 	public void setCLASSNAME(String cLASSNAME) {
 		CLASSNAME = cLASSNAME;
 	}
+
 	public String getUSERNAME() {
 		return USERNAME;
 	}
+
 	public void setUSERNAME(String uSERNAME) {
 		USERNAME = uSERNAME;
 	}
+
+	public String getCLASSPHOTO1() {
+		return CLASSPHOTO1;
+	}
+
+	public void setCLASSPHOTO1(String cLASSPHOTO1) {
+		CLASSPHOTO1 = cLASSPHOTO1;
+	}
+
+	public String getAllschedule() {
+		return Allschedule;
+	}
+
+	public void setAllschedule(String allschedule) {
+		Allschedule = allschedule;
+	}
+
 	
 	
 	
+	
+
 	
 }
--- OGO/src/main/java/com/pay/main/PaySuccess.java
+++ OGO/src/main/java/com/pay/main/PaySuccess.java
@@ -1,19 +1,23 @@
 package com.pay.main;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
+import com.dto.login.MemberDTO;
+import com.dto.pay.PayDTO;
+import com.dto.pay.paymentDTO;
 import com.service.pay.PayService;
 
-/**
- * Servlet implementation class PaySuccess
- */
 @WebServlet("/PaySuccess")
 public class PaySuccess extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -27,6 +31,8 @@ public class PaySuccess extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		PayService service = new PayService();
+		
 		String amount = request.getParameter("amount");
 		System.out.println(amount);
 		
@@ -38,11 +44,46 @@ public class PaySuccess extends HttpServlet {
 		if (amount != null && amount != "" && orderNum != null) {
 			PayService Pservice = new PayService();
 			int COS = Pservice.COS(orderNum);
-			System.out.println(COS);
-		}
+			System.out.println(COS); //결제 완료
+			session.removeAttribute("orderNum");
+			
+			MemberDTO dto = (MemberDTO) session.getAttribute("login");
+			List<PayDTO> list = service.getPaylist(dto.getUserId());
+			
+			for (PayDTO payDTO : list) {
+				List<String> SClist = new ArrayList<>();
+				SClist.add(payDTO.getSCHEDULE1());
+				SClist.add(payDTO.getSCHEDULE2());
+				SClist.add(payDTO.getSCHEDULE3());
+				SClist.add(payDTO.getSCHEDULE4());
+				SClist.add(payDTO.getSCHEDULE5());
+				SClist.add(payDTO.getSCHEDULE6());
+				SClist.add(payDTO.getSCHEDULE7());
+				SClist.add(payDTO.getSCHEDULE8());
+				SClist.add(payDTO.getSCHEDULE9());
+				SClist.add(payDTO.getSCHEDULE10());		
+				
+				String date = "";
+				for (int i = 0; i < SClist.size(); i++) {
+					if (SClist.get(i) != null) {
+						date += SClist.get(i)+",";
+					}//if
+				
+				System.out.println("하나씩 만들어서 date에 넣었어!"+date);
+				
+				payDTO.setAllschedule(date);
+				
+				}//for
+			}//list
+			
+			
+			
+			
+			session.setAttribute("PayList", list);
+		}//if 아이디가 있을 경우
 		
-		
-		 response.sendRedirect("/OGO/PaySuccess.jsp"); 
+		session.removeAttribute("orderNum");
+		response.sendRedirect("/OGO/PaySuccess.jsp"); 
 		
 	}
 
